9ebc0b682bc4ef5b323274a66ade1ef6077428f0
ovadya_s <ovadya.sokolov@gmail.com>
2019-10-07 11:02:11 +0300

    Project initialization

---
+++ src/test/java/com/artgallery/ArtgalleryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.artgallery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArtgalleryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
